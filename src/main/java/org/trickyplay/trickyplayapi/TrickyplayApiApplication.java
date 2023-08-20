de345fbf92d00caa62530d56e0bfd96d8034e3b9
Krzysztof Damian Basior <KrzysztofDamianBasior@gmail.com>
2023-08-20 14:25:37 +0200

    Initial commit

---
+++ src/main/java/org/trickyplay/trickyplayapi/TrickyplayApiApplication.java
@@ -0,0 +1,13 @@
+package org.trickyplay.trickyplayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrickyplayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrickyplayApiApplication.class, args);
+	}
+
+}
