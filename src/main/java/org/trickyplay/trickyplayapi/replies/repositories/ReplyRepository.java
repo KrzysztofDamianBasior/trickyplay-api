70fba6a27ce9f072a1197abac8f73bb29106c429
Krzysztof Damian Basior <KrzysztofDamianBasior@gmail.com>
2023-10-16 23:41:47 +0200

    feat: add methods that retrieve a page of responses and a method that retrieves responses belonging to the selected author

--- src/main/java/org/trickyplay/trickyplayapi/replies/repositories/ReplyRepository.java
+++ src/main/java/org/trickyplay/trickyplayapi/replies/repositories/ReplyRepository.java
@@ -1,11 +1,27 @@
 package org.trickyplay.trickyplayapi.replies.repositories;
 
-import org.trickyplay.trickyplayapi.replies.entities.Reply;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.trickyplay.trickyplayapi.replies.entities.Reply;
 
 import java.util.List;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
     List<Reply> findAllByParentCommentIdIn(List<Long> ids);
+
+    Page<Reply> findAll(Pageable page);
+
+    Page<Reply> findAllByParentCommentId(long parentCommentId, Pageable page);
+
+    Page<Reply> findAllByAuthorName(String authorName, Pageable page);
+
+    Page<Reply> findAllByAuthorId(long id, Pageable page);
+
+    List<Reply> findAllByIdIn(List<Long> ids); // where x.id in ?1
+
+    @Query("Select r from Reply r left join fetch r.author")
+    List<Reply> findAllRepliesWithAuthors(Pageable page);
 }
